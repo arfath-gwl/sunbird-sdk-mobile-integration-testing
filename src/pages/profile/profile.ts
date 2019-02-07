8f46abec1580393569fdcc463d7794c05eae63ee
arafath <arafath.ahmed@goodworklabs.com>
2019-02-07 17:03:49 +0530

    FIXED: COMPLETED: all offline methods of profile and group

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -69,9 +69,25 @@ export class ProfilePage {
     })
   }
 
-  getCurrentProfile() {
+  setCurrentProfileWithId() {
+    this.profileService.setCurrentProfile('71446508-45d7-4a92-899f-29e8a2c746d6').toPromise().then((result: any) => {
+      console.log(result)
+    }).catch((error: any) => {
+      console.log('error', error);
+    })
+  }
+
+  getCurrentProfileSession() {
     this.profileService.getCurrentProfileSession().toPromise().then((result: any) => {
       console.log('getCurrentProfileSession', result);
+    }).catch((error: any) => {
+      console.log('error', error);
+    })
+  }
+
+  getCurrentProfile() {
+    this.profileService.getCurrentProfile().toPromise().then((result: any) => {
+      console.log('getCurrentProfile', result);
     }).catch((error: any) => {
       console.log('error', error);
     })
@@ -81,7 +97,6 @@ export class ProfilePage {
     const request: UpdateServerProfileInfoRequest = {
       userId: 'dummyId',
       phone: '911'
-
     };
     this.profileService.updateServerProfile(request).toPromise().then((result: any) => {
       console.log('success while updating', result);
