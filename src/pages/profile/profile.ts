95324c45a254be64c8dec68182136dba0e3126c2
subranil <subranil.saha@tarento.com>
2019-02-07 17:25:54 +0530

    Add: Course page and oAuth login

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -27,19 +27,18 @@ export class ProfilePage {
 
   createProfile() {
     const req: Profile = {
-      uid: '1235',
-      handle: 'Hello',
+      uid: '',
+      handle: 'Shuvranil',
       board: ['cbse', 'icse', 'bhojpuri'],
-      created_at: Date.now(),
+      createdAt: Date.now(),
       grade: ['72', '77'],
       medium: ['ENGLISH'],
-      profile_type: ProfileType.TEACHER,
-      source: ProfileSource.LOCAL,
+      profileType: ProfileType.TEACHER,
+      source: ProfileSource.SERVER,
       subject: ['eng', 'maths'],
       syllabus: ["1oth"],
-      grade_value: {}
+      gradeValue: {'key': 'A+'}
     };
-
     this.profileService.createProfile(req).toPromise().then((res: any) => {
       console.log('CreatedProfile?--', res);
     }).catch((err: any) => {
@@ -48,12 +47,43 @@ export class ProfilePage {
   }
 
   deleteProfile() {
-    let uid = '12345';
+    let uid = '0286aa65-7519-479e-8ead-7784883d2d86';
     this.profileService.deleteProfile(uid).toPromise().then((result: any) => {
       console.log('successfully deleted.', result);
     }).catch((error: any) => {
       console.log('error', error);
     })
+  }
+
+  updateServerProfiles() {
+    const request: UpdateServerProfileInfoRequest = {
+      userId: 'd60b31bd-089c-4e9a-87ec-1bf859284964',
+      phone: '9019720923'
+    };
+    this.profileService.updateServerProfile(request).toPromise().then((result: any) => {
+      console.log('profile updated', result);
+    }).catch((error: any) => {
+      console.log('could not updated', error);
+    });
+  }
+
+  // getServerProfiles() {
+  //   const request: ServerProfileSearchCriteria = {
+  //     query: 'user',
+  //     identifiers: ['659b011a-06ec-4107-84ad-955e16b0a48a'],
+  //     fields: ['identifier', 'lastName', 'firstName'],
+  //     offset: 0,
+  //     limit: 10
+  //   };
+  //   this.profileService.getServerProfiles(request).toPromise().then((result: any) => {
+  //     console.log('ServerProfileSearchCriteria--', result);
+  //   }).catch((err: any) => {
+  //     console.log('error while searching server Profiles', err);
+  //   })
+  // }
+
+  getTenantInfo() {
+    // TODO
   }
 
   getAllProfile() {
@@ -69,9 +99,22 @@ export class ProfilePage {
     })
   }
 
+  getServerProfilesDetails() {
+    // TODO
+  }
+
+  setCurrentProfile() {
+    let uid = 'd60b31bd-089c-4e9a-87ec-1bf859284964';
+    this.profileService.setCurrentProfile(uid).toPromise().then((res: any) => {
+      console.log('set Current Profile--', res);
+    }).catch((err: any) => {
+      console.log('err while setting current profile', err);
+    });
+  }
+
   getCurrentProfile() {
-    this.profileService.getCurrentProfileSession().toPromise().then((result: any) => {
-      console.log('getCurrentProfileSession', result);
+    this.profileService.getCurrentProfile().toPromise().then((result: any) => {
+      console.log('getCurrentProfile', result);
     }).catch((error: any) => {
       console.log('error', error);
     })
