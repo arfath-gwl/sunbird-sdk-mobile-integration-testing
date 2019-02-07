38fba121b333945ef0775d81101ed6ed51697c51
arafath <arafath.ahmed@goodworklabs.com>
2019-02-07 10:12:23 +0530

    REFACTORED: group page

--- src/pages/group/group.ts
+++ src/pages/group/group.ts
@@ -1,6 +1,6 @@
-import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { GroupService } from 'sunbird-sdk';
+import {Component, Inject} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {GroupService} from 'sunbird-sdk';
 
 /**
  * Generated class for the GroupPage page.
@@ -9,7 +9,6 @@ import { GroupService } from 'sunbird-sdk';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-group',
   templateUrl: 'group.html',
@@ -17,24 +16,25 @@ import { GroupService } from 'sunbird-sdk';
 export default class GroupPage {
 
   constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    @Inject('GROUP_SERVICE') private groupService: GroupService) {
+              public navParams: NavParams,
+              @Inject('GROUP_SERVICE') private groupService: GroupService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GroupPage');
   }
 
-  createGroups() {
+  createGroup() {
     const query = {
-      gid: 'gr-1',
+      gid: '',
       name: 'mobile',
       syllabus: ['eng', 'phy'],
       createdAt: 2,
       grade: ['A', 'B'],
-      gradeValueMap: { "A": 'a' },
+      gradeValueMap: {"A": 'a'},
       updatedAt: 1,
-    }
+    };
+
     this.groupService.createGroup(query).subscribe((val) => {
       console.log(val);
     }, err => {
@@ -42,46 +42,60 @@ export default class GroupPage {
     })
   }
 
-  deleteGroups() {
-    this.groupService.deleteGroup('gr-1').subscribe((val) => {
+  deleteGroupWithId() {
+    this.groupService.deleteGroup('a7e2a30a-c170-4cce-851a-1ddab8f4e19d').subscribe((val) => {
       console.log(val);
     }, err => {
       console.log(err);
     })
   }
-  updateGroups() {
+
+  updateGroupWithId1() {
     const query = {
-      gid: 'gr-1',
+      gid: 'ed08d049-0ce6-4f7a-90cc-25a43c4a43d0',
       name: 'devops',
       syllabus: ['c', 'c++'],
       createdAt: 2,
       grade: ['A', 'B'],
-      gradeValueMap: { "A": 'a' },
+      gradeValueMap: {"A": 'a'},
       updatedAt: 1,
-    }
+    };
+
     this.groupService.updateGroup(query).subscribe((val) => {
       console.log(val);
     }, err => {
       console.log(err);
     })
   }
-  getAllGroups() {
+
+  getAllGroupsWithSpecificUserId() {
     const q = {
       uid: 'dfd4f1e5-c475-48c1-a5d2-5d245e684d12'
-    }
-    this.groupService.getAllGroup(q).subscribe((val) => {
+    };
+
+    this.groupService.getAllGroups(q).subscribe((val) => {
       console.log(val);
     }, err => {
       console.log(err);
     })
   }
-  addProfileToGroup(){
+
+  getAllGroups() {
+    this.groupService.getAllGroups().subscribe((val) => {
+      console.log(val);
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  addProfileToGroupWIthId2() {
     const q = {
-      groupId: 'gr-1',
-    uidList: ['dfd4f1e5-c475-48c1-a5d2-5d245e684d12', '7695bab4-6188-4557-8e95-31b41de8cf1c', '4bc2e57a-7ba8-46fc-b8f8-c6a867de3b8e']
-    }
+      groupId: '22dba91b-e3e5-42d9-92cd-890380206edd',
+      uidList: ['dfd4f1e5-c475-48c1-a5d2-5d245e684d12', '7695bab4-6188-4557-8e95-31b41de8cf1c', '4bc2e57a-7ba8-46fc-b8f8-c6a867de3b8e']
+    };
+
     this.groupService.addProfilesToGroup(q).subscribe((val) => {
-       console.log(val);
+      console.log(val);
     }, err => {
       console.log(err)
     })
